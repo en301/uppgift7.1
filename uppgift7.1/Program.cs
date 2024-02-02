f3f59bacb89d4daaadd5ad7d4d5c928646fd1ea5
en301 <emilsjostrand1000@gmail.com>
2024-02-02 12:14:24 +0100

    Add project files.

---
+++ uppgift7.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace uppgift_7_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string namn = "";
+            List<string> namnlista = new List<string>();
+            do
+            {
+                Console.WriteLine("Skriv in ett namn eller en tom rad om du vill avsluta");
+                namn = Console.ReadLine().ToLower();
+                if (namn != "")
+                {
+                    namnlista.Add(namn);
+                }
+            } while (namn != "");
+            namnlista.Sort();
+            Console.WriteLine("Namnen i bokstavsordning fast baklänges");
+            for (int i = namnlista.Count - 1; i >= 0; i--)
+            {
+                Console.WriteLine(namnlista[i]);
+            }
+        }
+    }
+}
